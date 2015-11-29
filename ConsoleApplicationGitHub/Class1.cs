147ffc35ee349dc3d5f1e87030fa598fb6ac99fa
babarqb <babarqb@gmail.com>
2015-11-29 13:42:22 +0500

    add new class1

--- ConsoleApplicationGitHub/Class1.cs
+++ ConsoleApplicationGitHub/Class1.cs
@@ -6,12 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplicationGitHub
 {
-    class Program
+    class Class1
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World");
-            Console.WriteLine("Hello World");
-        }
     }
 }
